9b9f8750ee22b6612a91ae537437a8dda6b3fa95
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-02-13 13:27:13 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,34 @@
+﻿// track
+using ConsoleApp24;
+
+Console.WriteLine("я калькулятор  могу  посчитать  площадь треугольника");
+while (true)
+{
+    StratProgramm();
+}
+static void StratProgramm()
+{
+    double x1 = GetDoubleForConsole("введите  длинну  стороны  а ");
+    double x2 = GetDoubleForConsole("введите  длинну  стороны  b ");
+    double x3 = GetDoubleForConsole("введите  длинну  стороны  c ");
+    double otvet = TriangleTools.GetSquare(x1, x2, x3);
+    Console.WriteLine("площадь  трегольника  равна: " + otvet);
+    Console.WriteLine("_____");
+}
+
+static double GetDoubleForConsole (string message)
+{
+    Console.WriteLine(message);
+    double x;
+    try
+    {
+        x= Convert.ToDouble(Console.ReadLine());
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message); 
+        Console.WriteLine("Попробуем еще раз...");
+        return GetDoubleForConsole(message);
+    }
+    return x;
+}
---
+++ ConsoleApp24/TriangleTools.cs
@@ -0,0 +1,26 @@
+﻿// track
+
+namespace ConsoleApp24
+{
+    internal class TriangleTools
+    {
+
+        public static double GetSquare (double a , double b , double c )
+        {
+            bool f = IsTryTriangle(a, b, c);
+            if (f == false)
+            {
+                return 0;
+            }
+            double p = (a + b + c ) / 2.0;
+            double s1 = p * (p-a ) *  (p-b) * (p-c); // нодо  взять  корень
+            double sTriang = Math.Sqrt( s1 );
+            return sTriang;
+        }
+
+        private static bool IsTryTriangle(double a, double b, double c)
+        {
+            return true;  // todo это не дописанно
+        }
+    }
+}
